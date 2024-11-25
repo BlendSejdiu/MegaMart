5c81d5ae115554b4d5fad0f64017dc609b08819c
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2024-11-25 19:41:43 +0100

    AppDbContext added and Category Controller Created

---
+++ MegaMart.Server/Controllers/Admin/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using MegaMart.Data;
+using MegaMart.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MegaMart.Server.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _db.Categories.ToListAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var category = await _db.Categories.FindAsync(id);
+            if (category == null)
+                return NotFound();
+            
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category obj)
+        {
+            if (obj == null)
+                return BadRequest(obj);
+
+            await _db.Categories.AddAsync(obj);
+            await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = obj.Id }, obj);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(int id, Category obj)
+        {
+            if (id != obj.Id || obj == null)
+                return BadRequest();
+            
+            var existingCategory = await _db.Categories.FindAsync(id);
+            if (existingCategory == null)
+                return NotFound();
+
+            _db.Entry(existingCategory).CurrentValues.SetValues(obj);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _db.Categories.FindAsync(id);
+            if (category == null)
+                return NotFound();
+
+            _db.Categories.Remove(category);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
