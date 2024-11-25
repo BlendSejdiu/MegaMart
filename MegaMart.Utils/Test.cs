569222ba93af96ae416a20cad5ee8035cde7ab4f
Gent Rechica <gr48119@ubt-uni.net>
2024-11-25 22:26:56 +0100

    Testing git conection

---
+++ MegaMart.Utils/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaMart.Utils
+{
+    public class Test
+    {
+        //TESTING
+        public int Id { get; set; }
+    }
+}
